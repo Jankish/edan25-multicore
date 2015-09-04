d6642066765871d19e1fb0f9bfc87d2a3f3ead96
Dalle <dalle@Dalles-MacBook-Pro.local>
2015-09-04 15:11:08 +0200

    new T1 and D1

--- lab1/ThreadService.java
+++ lab1/ThreadService.java
@@ -7,42 +7,27 @@ class ThreadService implements Runnable {
 	static ExecutorService executor;
 	private int nThread;
 	private LinkedList<Vertex> worklist;
-	
+
 	public ThreadService(LinkedList<Vertex> worklist, int nthread) {
 		nThread = nthread;
-		this.worklist = worklist; 
+		this.worklist = worklist;
 		executor = Executors.newFixedThreadPool(nThread);
 	}
-	
-	public static void multiThreadLiveness(LinkedList<Vertex> worklist)
-	{
-		Vertex			u;
-		Vertex			v;
-		int			i;
-		LinkedList<Vertex>	worklist;
-		long			begin;
-		long			end;
 
-		System.out.println("computing liveness...");
+	public static void multiThreadLiveness(LinkedList<Vertex> worklist) {
+		Vertex u;
 
-		begin = System.nanoTime();
-		worklist = new LinkedList<Vertex>();
-
-		for (i = 0; i < vertex.length; ++i) {
-			worklist.addLast(vertex[i]);
-			vertex[i].listed = true;
+		System.out.println("computing multi threaded liveness...");
+		synchronized (worklist) {
+			while (!worklist.isEmpty()) {
+				u = worklist.remove();
+				u.listed = false;
+				u.computeIn(worklist);
+			}
 		}
 
-		while (!worklist.isEmpty()) {
-			u = worklist.remove();
-			u.listed = false;
-			u.computeIn(worklist);
-		}
-		end = System.nanoTime();
-
-		System.out.println("T = " + (end-begin)/1e9 + " s");
-//		for (i = 0; i < vertex.length; ++i) 
-//			vertex[i].print();
+		// for (i = 0; i < vertex.length; ++i)
+		// vertex[i].print();
 	}
 
 	@Override
@@ -50,4 +35,4 @@ class ThreadService implements Runnable {
 		// TODO Auto-generated method stub
 		multiThreadLiveness(worklist);
 	}
-}	
+}
