11c8a5466364b0bfeb455022b84b922c021bb567
gda11 <dalle@Dalles-MacBook-Pro.local>
2015-09-30 10:03:55 +0200

    new sortD

--- lab3/sortD.c
+++ lab3/sortD.c
@@ -7,63 +7,60 @@
 #include <sys/times.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <string.h>
 
-//static size_t thread_count = 4;
-_Atomic int thread_count = 4;
-static void in_sort(
-		    void*		base,	// Array to sort.
-		    size_t		n,	// Number of elements in base.
-		    size_t		s,	// Size of each element.
-		    int		(*cmp)(const void*, const void*), // Behaves like strcmp
-		    pthread_t*	t, // thread array
-		    size_t		t_size);
+static size_t thread_count = 16;
+static int t_count = 0;
+pthread_t threads[16];
+int c = 0;
 
-
+typedef struct s_data
+{
+	double*		part;	// Array to sort.
+	int		size;	// Number of elements in base.
+	size_t		e_size;	// Size of each element.
+	int		(*cmp)(const void*, const void*);// Behaves like strcmp
+} s_data;
 
 static double sec(void)
 {
 	struct timeval t;
 	gettimeofday(&t, NULL);
 	return(t.tv_usec);
-	//return (1.0e-6*t.tv_usec + t.tv_sec);
 }
 
-static void deep_sort(
-	       void*		base,	// Array to sort.
-	       size_t		n,	// Number of elements in base.
-	       size_t		s,	// Size of each element.
-	       int		(*cmp)(const void*, const void*)) // Behaves like strcmp
+static void* deep_sort(void* d_segment)
 {
-	qsort(base, n, s, cmp);
+	s_data* data = (s_data*)d_segment;
+	qsort(data->part, data->size, data->e_size, data->cmp);
+	return NULL;
 }
 
-
-static void start_new_thread(
-		      pthread_t*	t_holder,
-		      size_t		holder_size,
-		      void		(*deep_sort)(const void*, const size_t*, const size_t*,
-						     const void*),
-		      void *		arg)
+static void* in_sort(void* data)
 {
-	pthread_t* pt = NULL;
-	int pos = holder_size - thread_count;
-	if (pos < holder_size) {
-		if(pthread_create(pt, NULL, (void*) deep_sort, &arg) != 0) {
+	s_data* d = (s_data*) data;
+	if (t_count < thread_count && d->size > 10) {
+		s_data ld, rd;
+		ld.part = d->part;
+		ld.size = (d->size / 2);
+		ld.e_size = d->e_size;
+		ld.cmp = d->cmp;
+	
+		rd.part = d->part + (d->size / 2);
+		rd.size = d->size - (d->size / 2);
+		rd.e_size = d->e_size;
+		rd.cmp = d->cmp;
+
+		if(pthread_create(&threads[t_count++], NULL, in_sort, (void*) &ld) != 0) {
 			fprintf(stderr, "Error: pthread creation went wrong\n");
 			exit(1);
 		}
-		t_holder[pos] = *pt;
-		thread_count--;
+		in_sort((void*) &rd);
+	} else {
+		deep_sort(data);
 	}
+	return NULL;
 }
-
-typedef struct s_data
-{
-	void*		base;	// Array to sort.
-	size_t		n;	// Number of elements in base.
-	size_t		s;	// Size of each element.
-	int		(*cmp)(const void*, const void*); // Behaves like strcmp
-} s_data;
 
 
 static void par_sort(
@@ -72,95 +69,77 @@ static void par_sort(
 	size_t		s,	// Size of each element.
 	int		(*cmp)(const void*, const void*)) // Behaves like strcmp
 {
-	pthread_t*	t[4];
-	in_sort(base, n, s, cmp, t, (size_t)4); // exchange 4 to some kind of variable
+	printf("PAR SORT\n\n");
+	int status;
+	s_data data;
+	data.part = base;
+	data.size = n;
+	data.e_size = s;
+	data.cmp = cmp;
+	in_sort((void*)&data);
+	
+	for (int i = 0; i < t_count; i++) {
+		status = pthread_join(threads[i], NULL);
+		if (status != 0)
+			printf("could not join with thread %d: %s\n", i, strerror(status));
+	}
+	deep_sort((void*)&data);
+//	qsort(data.part, data.size, data.e_size, data.cmp);
+	
 }
 
-static void in_sort(
-		void*		base,	// Array to sort.
-		size_t		n,	// Number of elements in base.
-		size_t		s,	// Size of each element.
-		int		(*cmp)(const void*, const void*), // Behaves like strcmp
-		pthread_t*	t, // thread array
-		size_t		t_size) //size of thread array
-{
-	size_t low	= 0;
-	size_t high	= s;
-	size_t mid	= 0;
-	
-	if (thread_count > 2) {
-		// Find new pivot
-		mid = (low + high) / 2; // 10 + 0 = 5 first time
-		void* left = (double*)base + (sizeof(double) + low);
-		size_t l_size = mid - low;
-		void* right = (double*)base + (sizeof(double) + mid);
-		size_t r_size = s - mid;
-		s_data* ld = {left, l_size, s, cmp};
-		s_data* rd = {right, r_size, s, cmp};
-		
-		// start two new threads and make the threads call par_sort
-		start_new_thread(t, 4, deep_sort, ld); //left
-		start_new_thread(t, 4, deep_sort, rd); //right
-		// (left and right)
-	} else if (thread_count > 0) {
-		// Find new pivot
-	
-		// start 1 new thread
-		// thread_count - 1
-	}
-//	qsort(base, n, s, cmp);
-	//join
-	//thread_count + 1
-}
+
 
 static int cmp(const void* ap, const void* bp)
 {	
 	/* you need to modify this function to compare doubles. */
 	const double*	a = ap;
 	const double*	b = bp;
-	return *a - *b;
-//	return 0;
+//	return *a - *b;
+	return (*a > *b) - (*a < *b);
 }
 
 int main(int ac, char** av)
 {
-	int		n = 20;
+	int		n = 100;
 	int		i;
-	double*		a;
+	double*		a, *b;
 	double		start, end;
-//	pthread*	t[4];
-
 	if (ac > 1)
 		sscanf(av[1], "%d", &n);
 
 	srand(getpid());
 
 	a = malloc(n * sizeof a[0]);
+	b = malloc(n * sizeof b[0]);
 	for (i = 0; i < n; i++)
-		a[i] = rand();
+		a[i] = b[i] = rand();
 	
-//	t = malloc(4 * sizeof(pthread));
-	
-	
-
-	start = sec();
 
 //#ifdef PARALLEL
+	start = sec();
+	printf("\nPARALLEL SORT\n\n");
 	par_sort(a, n, sizeof a[0], cmp);
-//#else
-//	qsort(a, n, sizeof a[0], cmp);
-//#endif
-
-	printf(" %*lf\n", 20, *(a+10));
+	end = sec();
 	printf("vector = \n");
 	for (int i = 0; i < n; i++)
 		printf(" %*lf\n", 30, *a++);
 	
-	end = sec();
-
 	printf("%1.2f micro s\n", end - start);
+//#else
+	start = sec();
+	printf("\nSEQUENTIAL SORT\n\n");
+	qsort(b, n, sizeof b[0], cmp);
+	end = sec();
+	printf("vector = \n");
+	for (int i = 0; i < n; i++)
+		printf(" %*lf\n", 30, *b++);
+	
+	printf("%1.2f micro s\n", end - start);
+//#endif
 
-	free(a);
-//	free(t);
+	
+
+//	free(a);
 	return 0;
 }
